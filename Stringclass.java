d982cc3307fdc2f08706a0a3d88ee2f0cf26b721
shubham Naikwade <shubham.naikwade2005@gmail.com>
2026-01-28 11:50:22 +0530

    Create Stringclass with string comparison examples
    Added a Stringclass with examples of string methods and operations.

---
+++ Stringclass.java
@@ -0,0 +1,21 @@
+//Strings class and comare the values
+class stringclass{
+    public static void main(String[]args){
+        //to compare two strings use equals method
+      //  String name1 ="shubham";
+       // String name2="shubham...";
+        //System.out.println(name1.equals(name2));//out:false
+        //we want to print character at index 2 in string name 1
+       // System.out.println(name1.charAt(2));//out:u
+       //..when we want to print formatted number we use printf()method 
+     //  System.out.printf("Formatted PI:%.2f",Math.PI);out:3.14
+    // System.out.printf("Hello my name is %s and I am %s","Shubham","student");
+     //out:Hello my name is Shubham and I am student
+     //operators in string class
+     //System.out.println('s'+'b');//out:213 it is converting s and b's value in ascii and adding 
+    // System.out.println('s'+3);//118
+   // System.out.println((char)('s'+3));//out:v
+    //System.out.println("a"+3);//out:a3 this is ame as after a few steps "a"+"1"
+        
+    }
+}
